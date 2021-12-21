de985a7a2c6a5f5340a4940f21cb14966c757270
ViktorElenich <velenic@mail.ru>
2021-12-21 17:56:30 +0300

    feat: add game page container and menu container

--- christmas-task/src/pages/game/game.ts
+++ christmas-task/src/pages/game/game.ts
@@ -1,18 +1,179 @@
 import { Page } from "../../core/templates/pages";
 
-export class Game extends Page {
+const tree = [
+    {
+        id: '1'
+    },
+    {
+        id: '2'
+    },
+    {
+        id: '3'
+    },
+    {
+        id: '4'
+    },
+    {
+        id: '5'
+    },
+    {
+        id: '6'
+    }
+];
 
-    static textObject = {
-        MainTitle: "Game Page",
-    };
+const backgroundImage = [
+    {
+        id: '1'
+    },
+    {
+        id: '2'
+    },
+    {
+        id: '3'
+    },
+    {
+        id: '4'
+    },
+    {
+        id: '5'
+    },
+    {
+        id: '6'
+    },
+    {
+        id: '7'
+    },
+    {
+        id: '8'
+    },
+    {
+        id: '9'
+    },
+    {
+        id: '10'
+    }
+];
+
+const garlandButtons = [
+    {
+        color: 'multicolor'
+    },
+    {
+        color: 'red'
+    },
+    {
+        color: 'green'
+    },
+    {
+        color: 'blue'
+    },
+    {
+        color: 'yellow'
+    }
+]
+
+export class Game extends Page {
 
     constructor(id: string) {
         super(id)
     }
 
+    renderWrapper(){
+
+        const linkToys = document.querySelector('.links:nth-child(2)');
+        const linkMain = document.querySelector('.links:nth-child(1)');
+        const linkGame = document.querySelector('.links:nth-child(3)');
+        linkMain.classList.remove('links-active');
+        linkToys.classList.remove('links-active');
+        linkGame.classList.add('links-active');
+
+        const wrapperGame = document.createElement('div');
+        wrapperGame.classList.add('wrapper-game');
+
+        const menuChoose = document.createElement('div');
+        menuChoose.classList.add('menu-choose');
+        wrapperGame.append(menuChoose);
+
+        const treeContainer = document.createElement('div');
+        treeContainer.classList.add('tree-container');
+        wrapperGame.append(treeContainer);
+
+        const menuFavorites = document.createElement('div');
+        menuFavorites.classList.add('menu-favorites');
+        wrapperGame.append(menuFavorites);
+
+        const audioAndSnowContainer = document.createElement('div');
+        audioAndSnowContainer.classList.add('audio-snow-container', 'menu');
+        menuChoose.append(audioAndSnowContainer);
+
+        const treeChooseContainer = document.createElement('div');
+        treeChooseContainer.classList.add('tree-choose-container', 'menu');
+        menuChoose.append(treeChooseContainer);
+
+        const bgContainer = document.createElement('div');
+        bgContainer.classList.add('bg-container', 'menu');
+        menuChoose.append(bgContainer);
+
+        const garlandContainer = document.createElement('div');
+        garlandContainer.classList.add('garland-container', 'menu');
+        menuChoose.append(garlandContainer);
+
+        const audioItem = document.createElement('div');
+        audioItem.classList.add('audio-item');
+        audioAndSnowContainer.append(audioItem);
+
+        const snowItem = document.createElement('div');
+        snowItem.classList.add('snow-item');
+        audioAndSnowContainer.append(snowItem);
+
+        tree.forEach(item => {
+            const treeItems = document.createElement('div');
+            treeItems.classList.add('tree-item');
+            treeItems.dataset.id = item.id;
+            treeChooseContainer.append(treeItems);
+        })
+
+        backgroundImage.forEach(item => {
+            const bgItems = document.createElement('div');
+            bgItems.classList.add('bg-item');
+            bgItems.dataset.id = item.id;
+            bgContainer.append(bgItems);
+        })
+
+        const garlandBtnContainer = document.createElement('div');
+        garlandBtnContainer.classList.add('garland-btn-container');
+        garlandContainer.append(garlandBtnContainer);
+
+        garlandButtons.forEach(item => {
+            const btnGarland = document.createElement('div');
+            btnGarland.classList.add('garland-btn', item.color);
+            btnGarland.dataset.color = item.color;
+            garlandBtnContainer.append(btnGarland);
+        })
+
+        const switchOffOn = document.createElement('div');
+        switchOffOn.classList.add('switch-off-on');
+        garlandContainer.append(switchOffOn);
+
+        const inputSwitch = document.createElement('input');
+        inputSwitch.classList.add('input-switch');
+        inputSwitch.type = 'checkbox';
+        inputSwitch.id = 'input-switch';
+        inputSwitch.checked = true;
+        switchOffOn.append(inputSwitch);
+
+        const labelSwitch = document.createElement('label');
+        labelSwitch.setAttribute('for', 'input-switch');
+        labelSwitch.dataset.on = 'Вкл.';
+        labelSwitch.dataset.off = 'Выкл.';
+        switchOffOn.append(labelSwitch);
+
+        this.container.append(wrapperGame);
+
+    }
+
     render(){
-        const title = this.creatHeaderTitle(Game.textObject.MainTitle);
-        this.container.append(title);
+        this.renderWrapper();
         return this.container;
     }
 }
